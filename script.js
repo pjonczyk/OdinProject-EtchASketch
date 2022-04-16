97611c7b65e2b55e5b7eb9dc6738d57fc396b08e
Philipp Jonczyk <philjonczyk@gmail.com>
2022-04-16 13:55:45 +0200

    added color picker and toggle gridlines functionality

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let gridSize = 25;
+const black = '#000000';
+const white = '#FFFFFF';
+let currentPaintingColor = black;
+let currentFillColor = white;
 
 function drawGrid(gridContainer) {
     for (i = 0; i < gridSize; i++) {
         for (j = 0; j < gridSize; j++) {
-            let newDiv = document.createElement("div");
-            newDiv.addEventListener('click', handleClick);
-            newDiv.classList.add('gridItem');
+            let newDiv = document.createElement('div');
+            newDiv.addEventListener('mousedown', handleClick);
+            newDiv.classList.toggle('gridItem');
+            newDiv.classList.toggle('gridItemBorderColor');
             gridContainer.appendChild(newDiv);
         }
     }
@@ -17,8 +22,15 @@ function clearChildren(parent) {
     }
 }
 
+function clearGrid() {
+    let gridItems = document.querySelectorAll('.gridItem');
+    gridItems.forEach(gridItem => {
+        gridItem.style.backgroundColor = white;
+    });
+}
+
 function handleClick(e) {
-    e.target.style.backgroundColor = 'black';
+    e.target.style.backgroundColor = currentPaintingColor;
 }
 
 function onSliderInput(e) {
@@ -30,12 +42,31 @@ function onSliderInput(e) {
     gridContainer.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
 }
 
+function onChangeColorPicker(e) {
+    currentPaintingColor = e.target.value;
+}
+
+function toggleGridlines(e) {
+    let gridItems = document.querySelectorAll('.gridItem');
+    gridItems.forEach(gridItem => {
+        gridItem.classList.toggle('gridItemBorderColor');
+    });
+    e.target.classList.toggle('gridlinesButtonActive');
+}
+
 function initialSetup() {
     let gridContainer = document.querySelector('.gridContainer');
     drawGrid(gridContainer);
 
     let slider = document.getElementById("myRange");
     slider.addEventListener('mouseup', onSliderInput);
+    let clearButton = document.getElementById("resetButton");
+    clearButton.addEventListener('click', clearGrid);
+    let colorPicker = document.getElementById("colorPicker");
+    colorPicker.addEventListener("change", onChangeColorPicker);
+    let gridlinesButton = document.getElementById("gridlinesButton");
+    gridlinesButton.addEventListener('click', toggleGridlines);
+
 }
 
 initialSetup();
